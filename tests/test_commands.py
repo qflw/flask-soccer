cfe371c1b7dcb5212945b416db3b8be321f0c0c4
qujack <qujack@gmail.com>
2020-06-04 14:25:10 +0200

    enhanced test commands

---
+++ tests/test_commands.py
@@ -0,0 +1,26 @@
+import unittest
+
+from soccer import create_app, db
+from soccer.model import User, Role, Permission
+
+
+class CommandTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_command_invalid(self):
+        response = self.client.get('/action/invalid')
+        self.assertEqual(response.status_code, 404)
+
+    def test_command_update(self):
+        response = self.client.get('/action/update')
+        self.assertEqual(response.status_code, 302)
